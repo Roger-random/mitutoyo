75a4b6c39895f40a87ef555e781d8545307d646c
Roger Cheng <roger.random@outlook.com>
2020-07-30 02:09:41 -0700

    Reduce string conversion, add units, other fixes

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,33 @@
 
 #include <Arduino.h>
 
-int req = 5; //mic REQ line goes to pin 5 through q1 (arduino high pulls request line low)
-int dat = 2; //mic Data line goes to pin 2
-int clk = 3; //mic Clock line goes to pin 3
+int req = 5; //Arduino pin for REQ line, drives transistor to ground SPC port pin 5 (~REQ)
+int dat = 2; //Arduino pin for Data line, connects directly to SPC port pin 2 (DATA)
+int clk = 3; //Arduino pin for Clock line, connects directly to SPC port pin 3 (~CLK)
+
 int i = 0;
 int j = 0;
 int k = 0;
-int signCh = 8;
-int sign = 0;
-int decimal;
-float dpp;
-int units;
 
-byte mydata[14];
-String value_str;
-long value_int; //was an int, could not measure over 32mm
-float value;
+// Mitutoyo SPC data port transmits 13 4-bit values
+// Byte Meaning
+// 0    Header, always 0xF
+// 1    Header, always 0xF
+// 2    Header, always 0xF
+// 3    Header, always 0xF
+// 4    Sign. Zero is positive. 0x8 is negative.
+// 5    Most significant digit (MSD) of measurement
+// 6    Next digit of measurement
+// 7    Next digit of measurement
+// 8    Next digit of measurement
+// 9    Next digit of measurement
+// 10   Least significant digit (LSD) of measurement
+// 11   Digit to place decimal point
+// 12   Unit. Zero is mm. Nonzero (b1000 is 1?) is inch
+
+byte spcdata[13]; // The raw data sent by instrument
+float value;      // The value calculated from raw data
+int decimal;      // Number of digits that are after decimal point
 
 void setup() {
     Serial.begin(9600);
@@ -32,6 +43,11 @@ void loop() {
 
     for( i = 0; i < 13; i++ ) {
         k = 0;
+
+        // Timing diagram indicates data bit has been valid for about 120 
+        // microseconds before the clock signal is raised, and remains
+        // valid for about 120 microseconds afterwards. This code reads data
+        // bit at the falling clock edge.
         for (j = 0; j < 4; j++) {
             while( digitalRead(clk) == LOW) {
             } // hold until clock is high
@@ -39,31 +55,38 @@ void loop() {
             } // hold until clock is low
             bitWrite(k, j, (digitalRead(dat) & 0x1));
         }
-        mydata[i] = k;
+
+        // After reading the first 4-bit value, we can drop REQ output.
+        if (i == 0) {
+            digitalWrite(req,LOW);
+        }
+        spcdata[i] = k;
     }
 
-    sign = mydata[4];
-    value_str = String(mydata[5]) + String(mydata[6]) + String(mydata[7]) + String(mydata[8] + String(mydata[9] + String(mydata[10]))) ;
-    decimal = mydata[11];
-    units = mydata[12];
-
-    value_int = value_str.toInt();
-    if (decimal == 0) dpp = 1.0;
-    if (decimal == 1) dpp = 10.0;
-    if (decimal == 2) dpp = 100.0;
-    if (decimal == 3) dpp = 1000.0;
-    if (decimal == 4) dpp = 10000.0;
-    if (decimal == 5) dpp = 100000.0;
-
-    value = value_int / dpp;
-
-    if (sign == 0) {
-        Serial.println(value,decimal);
-    }
-    if (sign == 8) {
-        Serial.print("-"); Serial.println(value,decimal);
+    // Combine measurement digits into a number
+    value = 0;
+    for( i = 5; i <= 10; i++) {
+        value *= 10;
+        value += spcdata[i];
     }
 
-    digitalWrite(req,LOW);
-    delay(100);
+    // Adjust number for decimal point position
+    decimal = spcdata[11];
+    value /= pow(10,decimal);
+
+    // Adjust if number is negative
+    if (spcdata[4] == 0x8) {
+        value *= -1;
+    }
+
+    // Print resulting value to serial port, to specified
+    // number of digits after decimal point.
+    Serial.print(value,decimal);
+
+    // Append units for value
+    if (spcdata[12] == 0) {
+        Serial.println(" mm");
+    } else {
+        Serial.println(" in");
+    }
 }
