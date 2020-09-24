fbff1356e5769669af0797d3d01e248387d93133
Roger Cheng <roger.random@outlook.com>
2020-09-23 21:58:26 -0700

    Act as stand-in for a Fowler indicator with USB cable

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,30 @@
 #include <Arduino.h>
 
 // When defined, will send SPC data as hexadecimal over serial.
-#define HEX_DATA_OUTPUT
+// #define HEX_DATA_OUTPUT
 
 // When defined, will convert raw data to human readable string
 // and send over serial.
-// #define HUMAN_READABLE_OUTPUT
+#define HUMAN_READABLE_OUTPUT
+
+#ifdef HUMAN_READABLE_OUTPUT
+#ifndef HEX_DATA_OUTPUT
+// When defined, will pretend to be a Fowler/Sylvak indicator
+// connected with the USB proximity data cable. Follows observed
+// behavior which is a little different from what was in the manual.
+//  * Use serial port parameters 4800 7E2
+//  * Will wait for an incoming byte before sending value
+//    Response is always 10 bytes.
+//  * Value always starts with a sign - positive or negative
+//  * Next 8 characters will be the number
+//    One of those characters is a decimal point
+//  * Final (10th) character is the \r termination
+//
+//  * HEX_DATA_OUTPUT must NOT be defined
+//  * HUMAN_READABLE_OUTPUT must be defined
+#define FOWLER_PLACEHOLDER
+#endif
+#endif
 
 int req = 5; //Arduino pin for REQ line, drives transistor to ground SPC port pin 5 (~REQ)
 int dat = 2; //Arduino pin for Data line, connects directly to SPC port pin 2 (DATA)
@@ -40,8 +59,17 @@ float value;      // The value calculated from raw data
 int decimal;      // Number of digits that are after decimal point
 #endif
 
+#ifdef FOWLER_PLACEHOLDER
+byte readBuffer[1];
+#endif
+
 void setup() {
+#ifdef FOWLER_PLACEHOLDER
+    Serial.begin(4800, SERIAL_7E2);
+    Serial.setTimeout(20); // Short timeout on read.
+#else
     Serial.begin(9600);
+#endif
     pinMode(req, OUTPUT);
     pinMode(clk, INPUT_PULLUP);
     pinMode(dat, INPUT_PULLUP);
@@ -49,6 +77,14 @@ void setup() {
 }
 
 void loop() {
+#ifdef FOWLER_PLACEHOLDER
+    while (Serial.available() < 1) {
+        // Wait for a byte to be sent from computer
+    }
+    // What was sent appears to be irrelevant, so we never look at it.
+    Serial.readBytes(readBuffer, 1);
+#endif
+
     digitalWrite(req, HIGH); // generate set request
 
     for( i = 0; i < 13; i++ ) {
@@ -100,17 +136,37 @@ void loop() {
         value *= -1;
     }
 
+    if (value >= 0) {
+        Serial.print("+");
+    }
     // Print resulting value to serial port, to specified
     // number of digits after decimal point.
     Serial.print(value,decimal);
+#ifdef FOWLER_PLACEHOLDER
+    // Now we have to calculate how many zeros to pad out the end.
+    // NOTE: This breaks down if value is >=100 or <=-100
+    //      (Only works with one or two digits before decimal point.)
+    int zeroPadding = 5-decimal;
 
+    if (value < 10 && value > -10) {
+        // Extra zero padding for single digit before decimal.
+        zeroPadding++;
+    }
+    for (int i = 0; i < zeroPadding; i++) {
+        Serial.print("0");
+    }
+    Serial.print("\r");
+#else
     // Append units for value
     if (spcdata[12] == 0) {
         Serial.print(" mm");
     } else {
         Serial.print(" in");
     }
+#endif // FOWLER_PLACEHOLDER
 #endif // HUMAN_READABLE_OUTPUT
 
+#ifndef FOWLER_PLACEHOLDER
     Serial.println();
+#endif
 }
